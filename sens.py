aa90316a1e5984a0751639c6fdf2a8c32aae94ba
Laura de Ruiter <laura@myname.nl>
2013-01-06 16:58:57 +0100

    Add logging to MySQL

--- sens.py
+++ sens.py
@@ -4,6 +4,8 @@ from Adafruit_CharLCD import Adafruit_CharLCD
 from time import localtime, strftime, sleep
 import RPi.GPIO as GPIO
 import sys
+import mysql.connector
+from mysql.connector import errorcode
 
 dataPin = 7
 clkPin = 12
@@ -34,7 +36,30 @@ while True:
   # print("T: {:2.2f}\xC2\xB0C H: {:2.2f}%\nD: {:2.2f}\xB0C T: {}".format(temperature, humidity, dewPoint, curtime))
   print("Date: {} Time: {} Temperature: {:2.2f}\xC2\xB0C Humiditiy: {:2.2f}% Dew Point: {:2.2f}\xC2\xB0C".format(curdate, curtime, temperature, humidity, dewPoint))
   
-  # Flush output, needed to get output if ran with nohup 
+  # Flush output, needed to get output if ran with nohup
   sys.stdout.flush()
 
+  try:
+# make connection with mysql, error handeling
+    conn = mysql.connector.connect(user="user", passwd="password", db="database")
+  except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCES_DENIED_ERROR:
+      print ("Wrong username or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+      print ("Wrong database name")
+    else:
+      print (err)
+
+# put data in database
+  cursor = conn.cursor()
+  add_data = ("insert into sensor_readings (temperature, humidity) values (%s, %s)")
+  sensor_data = (temperature, humidity)
+  cursor.execute(add_data, sensor_data)
+
+# commit data and close connection with mysql
+  conn.commit()
+  cursor.close()
+  conn.close()
+
   sleep(60)
+
