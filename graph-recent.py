8ab7ed14112369cb8d9b698252b76f1e3f881cf9
Daniël van Eeden <git@myname.nl>
2013-09-22 11:15:59 +0200

    Add graph data from forecast.io

--- graph-recent.py
+++ graph-recent.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 from __future__ import print_function
 from pylab import *
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 import time
 import mysql.connector
 import sys
 import json
 import requests
 import ConfigParser
+import forecastio
 
 config = ConfigParser.ConfigParser()
 config.read('rpi-hygro.cnf')
@@ -55,10 +56,37 @@ for wd in jdat['list']:
     wtime.append(datetime.fromtimestamp(int(wd['dt'])))
     whum.append(wd['main']['humidity'])
 
+# Data from forecast.io
+api_key = config.get('forecastio', 'api_key')
+lat = config.get('forecastio', 'lat')
+lng = config.get('forecastio', 'lng')
+
+now = datetime.now()
+startdate = now - timedelta(2)
+
+mindate = d[0]
+maxdate = d[-1]
+
+fchum = []
+fctmp = []
+fcdat = []
+for date_add in range(3):
+    day = startdate + timedelta(date_add)
+    print("Fetching forecast.io data for %s" % day)
+    forecast = forecastio.load_forecast(api_key, lat, lng, day)
+    hourlyfc = forecast.hourly()
+    for datapoint in hourlyfc.data:
+	if mindate < datapoint.time < maxdate:
+            fchum.append(datapoint.humidity * 100)
+            fctmp.append(datapoint.temperature)
+            fcdat.append(datapoint.time)
+
 title('Humidity and Temperature')
 grid(True)
 p1 = plot_date(d, mhum, 'r-', label='Humidity')
 p2 = plot_date(d, mtmp, 'b-', label='Temperature')
 p3 = plot_date(wtime, whum, 'g-', label='Humidity OWM')
-legend()
+p3 = plot_date(fcdat, fctmp, 'y-', label='Temperature FCIO')
+p3 = plot_date(fcdat, fchum, 'c-', label='Humidity FCIO')
+legend(loc=6)
 show()
